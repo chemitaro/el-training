5418e091776317ad04c5c79c4d68cd52f6c3f12c
yuta iwasawa <iwasi_44@hotmail.com>
2021-10-20 10:54:14 +0900

    task モデル、コントローラー追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    
+  end
+  def show
+    
+  end
+  def edit
+    
+  end
+  def update
+    
+  end
+  def destroy
+    
+  end
+end
